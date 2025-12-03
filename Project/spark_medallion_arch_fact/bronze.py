8d5a7611d58444b5489ec21d9784b8284ce39a5e
Sarvesh Jathar <sarveshjathar448@gmail.com>
2025-12-03 16:14:04 +0530

    Create bronze.py to load order items data
    Defined schema for order items and loaded data from CSV.

---
+++ Project/spark_medallion_arch_fact/bronze.py
@@ -0,0 +1,35 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, BooleanType
+import pyspark.sql.functions as F
+
+catalog_name = 'ecom'
+
+order_items_schema = StructType([
+    StructField("dt",                 StringType(), True),
+    StructField("order_ts",           StringType(), True),
+    StructField("customer_id",        StringType(), True),
+    StructField("order_id",           StringType(), True),
+    StructField("item_seq",           StringType(), True),
+    StructField("product_id",         StringType(), True),
+    StructField("quantity",           StringType(), True),
+    StructField("unit_price_currency",StringType(), True),
+    StructField("unit_price",         StringType(), True),
+    StructField("discount_pct",       StringType(), True),
+    StructField("tax_amount",         StringType(), True),
+    StructField("channel",            StringType(), True),
+    StructField("coupon_code",        StringType(), True),
+])
+
+# Load data using the schema defined
+raw_data_path = "/Volumes/ecom/source_data/raw/order_items/landing/*.csv"
+
+df = spark.read.option("header", "true").option("delimiter", ",").schema(order_items_schema).csv(raw_data_path) \
+    .withColumn("file_name", F.col("_metadata.file_path")) \
+    .withColumn("ingest_timestamp", F.current_timestamp())
+
+display(df.limit(5))
+
+df.write.format("delta") \
+    .mode("overwrite") \
+    .option("mergeSchema", "true") \
+    .saveAsTable(f"{catalog_name}.bronze.brz_order_items")
+
